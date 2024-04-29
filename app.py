a98c95c926e83d1a54b6f3bdc8629c43e38b0e32
sinjy1203 <sinjy1203@gmail.com>
2024-04-29 13:00:48 +0900

    Feat: addData test

--- app.py
+++ app.py
@@ -12,24 +12,25 @@ app = FastAPI()
 async def root(request: dict):
     req = dict(request)
     req_text = req["userRequest"]["utterance"]
+    print(req_text)
+    if req_text.startswith("/데이터입력"):
+        response_text = "데이터 입력이 완료하였습니다."
+    else:
+        response = ollama.chat(
+            model="EEVE-Korean-10.8B",
+            messages=[
+                {
+                    "role": "user",
+                    "content": req_text,
+                },
+            ],
+        )
+        response_text = response["message"]["content"].strip()
 
-    response = ollama.chat(
-        model="EEVE-Korean-10.8B",
-        messages=[
-            {
-                "role": "user",
-                "content": req_text,
-            },
-        ],
-    )
-
+    print(response_text)
     res = {
         "version": "2.0",
-        "template": {
-            "outputs": [
-                {"simpleText": {"text": response["message"]["content"].strip()}}
-            ]
-        },
+        "template": {"outputs": [{"simpleText": {"text": response_text}}]},
     }
 
     return res
