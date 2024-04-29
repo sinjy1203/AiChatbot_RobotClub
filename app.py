9353f1960a8543072a5b417593bbec6ff13b3781
sinjy1203 <sinjy1203@gmail.com>
2024-04-29 21:16:52 +0900

    feat: RAG 구현

--- app.py
+++ app.py
@@ -1,9 +1,47 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.documents import Document
+from langchain_chroma import Chroma
+from langchain_community.llms import Ollama
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
 from fastapi import FastAPI, HTTPException
+import re
 import json
 import uvicorn
 from pydantic import BaseModel
 
 import ollama
+
+embeddings = HuggingFaceEmbeddings(model_name="jhgan/ko-sroberta-multitask")
+vectorstore = Chroma(embedding_function=embeddings)
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
+TEMPLATE = """
+Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {prompt}
+"""
+prompt = PromptTemplate.from_template(TEMPLATE)
+
+llm = Ollama(model="EEVE-Korean-10.8B:v2")
+
+
+def format_docs(docs):
+    return "\n".join([doc.page_content for doc in docs])
+
+
+rag_chain = (
+    {"context": retriever | format_docs, "prompt": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
 
 app = FastAPI()
 
@@ -14,19 +52,15 @@ async def root(request: dict):
     req_text = req["userRequest"]["utterance"]
     print(req_text)
     if req_text.startswith("/데이터입력"):
+        pattern = "^" + re.escape("/데이터입력")
+        req_text = re.sub(pattern, "", req_text).strip()
+
+        retriever.add_documents([Document(page_content=req_text)])
         response_text = "데이터 입력이 완료하였습니다."
     else:
-        response = ollama.chat(
-            model="EEVE-Korean-10.8B",
-            messages=[
-                {
-                    "role": "user",
-                    "content": req_text,
-                },
-            ],
-        )
-        response_text = response["message"]["content"].strip()
+        response_text = rag_chain.invoke(req_text)
 
+    response_text = response_text.strip()
     print(response_text)
     res = {
         "version": "2.0",
