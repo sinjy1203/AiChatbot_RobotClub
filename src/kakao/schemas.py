d98bf8bc8d8cd2b25a6cb3cc5525eaa16deb5d96
sinjy1203 <sinjy1203@gmail.com>
2024-05-14 13:41:17 +0900

    edit: kakao api server schemas

--- src/kakao/schemas.py
+++ src/kakao/schemas.py
@@ -6,7 +6,7 @@ class UserRequest(BaseModel):
     timezone: str = "Asia/Seoul"
     params: Dict[str, str] = {"ignoreMe": "true"}
     block: Dict[str, str] = {"id": "py9qmysqdr68fn9h2oaif7it", "name": "블록 이름"}
-    utterance: str = "/데이터입력 안뇽"
+    utterance: str = "/데이터입력 로보트연구회 회장은 홍길동이야"
     lang: Optional[str] = None
     user: Dict = {}
 
